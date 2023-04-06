e029f0c7d169f4f82a34730f7cecf628360c1e49
fenaldyap <93239146+fenaldyap@users.noreply.github.com>
2023-04-07 00:41:04 +0700

    finalized

--- src/test/java/openway/AppTest.java
+++ src/test/java/openway/AppTest.java
@@ -5,8 +5,10 @@ package openway;
 
 import java.util.Scanner;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AppTest 
@@ -14,12 +16,36 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    CalculationLogic calcLogic;
+   CalculationLogic calcLogic;
 
-     @Test
-     public void testingCalculation(){
-         calcLogic = new CalculationLogic(3, 1);
-         System.out.println("Result : "+calcLogic.Calculating());
-     }
+   @DataProvider (name = "data-provider")
+   public Object[][] dataProvider(){
+      return new Object[][] {{-2, -0.5},
+                              {-8, -0.125},
+                              {4, 0.25}, 
+                              {10, 0.1},
+                              {0, 1.0 / 0.0},
+                              {+4, 0.25},
+                              {+100, 0.01},
+                              {1000, 0.001},
+                              {10000, +0.0001}};           
+   }
+
+   @Test(priority = 1, dataProvider = "data-provider")
+   public void testingValidCalculation(int inputParameter, double expectedResult){
+      calcLogic = new CalculationLogic(inputParameter, 1);
+      Assert.assertEquals(calcLogic.Calculating(), expectedResult);
+   }
+
+   @Test(priority = 2)
+   public void testingInvalidCalculationGivenString(){
+      try{
+         String input = "test";
+         calcLogic = new CalculationLogic(Integer.parseInt(input), 1);
+         System.out.println(calcLogic.Calculating());
+      }catch (Exception e){
+         Assert.assertEquals(e.toString(), "java.lang.NumberFormatException: For input string: \"test\"");
+      }
+   }
 
 }
